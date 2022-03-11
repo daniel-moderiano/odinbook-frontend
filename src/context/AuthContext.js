97c66f8a7d99008fc877e56c62cf5558bf63446d
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-11 21:23:47 +1030

    Expand authReducer function with expected actions

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,19 +1,37 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 export const AuthContext = createContext();
 
+// Reducer function to handle different auth-related actions. Typically the payload in each case will be a user object
 export const authReducer = (state, action) => {
   switch (action.type) {
-  
+    case 'LOGIN':
+      return { ...state, user: action.payload }
+
+    // Remove user object on logout action dispatch
+    case 'LOGOUT':
+      return { ...state, user: null }
+
+    // Used to set the authIsReady property before initial rendering of any components. This allows conditional rendering
+    case 'AUTH_IS_READY':
+      return { ...state, user: action.payload, authIsReady: true }
+
     default:
       return state;
   }
 }
 
 export const AuthContextProvider = ({ children }) => {
+  // Initialise the state to be controlled by the authReducer function above. The dispatch function is made available to components via the AuthContextProvider below, which wraps the app
   const [state, dispatch] = useReducer(authReducer, {
-    uesr: null,
-  })
+    user: null,
+    authIsReady: false,
+  });
+
+  // Ensure an empty dependency array is maintained. This hook should only be evaluated once on initial component render
+  useEffect(() => {
+    // Check auth state here and dispatch AUTH_IS_READY action (use getCurrentUser API call)
+  }, []);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
