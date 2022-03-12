14620d14753e3f10c11f000eb956b275f80bd2bc
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-12 17:34:24 +1030

    Add route protection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
 import Home from './components/Home';
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  // Presence of user will allow conditional rendering of routes/route protection on frontend
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
+      {authIsReady && (<>
         <Routes>
-          <Route path="/" element={<Signup />}/>
-          <Route path="/signup" element={<Signup />}/>
-          <Route path="/login" element={<Login />}/>
-          {/* <Route path="/signup" />
-          <Route path="/login" /> */}
+          <Route path="/" element={
+            <>
+            {!user && <Navigate to="/login" />}
+            {user && <Home />}
+            </>
+          } />       
+          <Route path="/signup" element={
+            <>
+            {!user && <Signup />}
+            {user && <Navigate to="/" />}
+            </>
+          }/>
+          <Route path="/login" element={
+            <>
+            {!user && <Login />}
+            {user && <Navigate to="/" />}
+            </>
+          }/>
         </Routes>
+      </>)}
     </div>
   );
 }
