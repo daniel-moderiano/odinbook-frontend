f38b2fc632d9453c20b3e215c395ad62a27fc7a7
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-14 11:30:34 +1030

    Style desktop login page

--- src/components/StyledLink.js
+++ src/components/StyledLink.js
@@ -18,7 +18,7 @@ const StyledLink = ({ children, to, design, onClick, customStyles }) => {
         break;
 
       case 'btn-secondary':
-        CSS = 'w-full text-center px-2 py-2 text-sm font-medium bg-teal-650 shadow-md text-white hover:bg-teal-550 md:text-base focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-teal-650/30'
+        CSS = 'flex items-center justify-center w-full px-4 py-2 font-semibold bg-teal-650 shadow-md text-white text-md md:text-lg hover:bg-teal-550 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-teal-650/30'
         break;
     
       default:
