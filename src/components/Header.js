c36e7ea3fcbc6c79da627d8f7c6ef194622ff867
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-14 16:45:47 +1030

    Adjust header to grid layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import Nav from "./Nav"
 
 const Header = () => {
   return (
-    <header role="banner" aria-labelledby="odinbook-logo" className="shadow-sm sticky top-0 w-full bg-white lg:flex items-center justify-between">
+    // <header role="banner" aria-labelledby="odinbook-logo" className="shadow-sm sticky top-0 w-full bg-white flex items-center justify-evenly lg:justify-between">
 
-      <div className="items-center hidden lg:flex">
+    <header role="banner" aria-labelledby="odinbook-logo" className="shadow-sm sticky top-0 w-full bg-white grid grid-cols-1 lg:grid-cols-3 items-center">
+
+      <div className="items-center hidden lg:flex justify-self-start">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105.22 83.68" className="w-20">
           <title id="odinbook-logo" aria-labelledby="optomrx-logo">Odinbook</title>
           <g id="Layer_2" data-name="Layer 2">
@@ -20,6 +22,15 @@ const Header = () => {
 
       <Nav />
       {/* Dropdown profile/settings menu here */}
+
+      {/* Separate logout button on the far right for large screens only (ordinarily part of nav menu) */}
+      <div className="hidden lg:block justify-self-end">
+        {/* Icon */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-8">
+          <path fill="#51557d" d="M160 416H96c-17.67 0-32-14.33-32-32V128c0-17.67 14.33-32 32-32h64c17.67 0 32-14.33 32-32S177.7 32 160 32H96C42.98 32 0 74.98 0 128v256c0 53.02 42.98 96 96 96h64c17.67 0 32-14.33 32-32S177.7 416 160 416zM502.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L402.8 224H192C174.3 224 160 238.3 160 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C515.1 266.1 515.1 245.9 502.6 233.4z"/>
+        </svg>
+        <span className="text-xs">Log out</span>
+      </div>
     </header>
   )
 }
