316688de667b1f05b0e4b358ed44dc04dffffbd3
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-22 12:47:17 +1030

    Add home link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,16 @@
 import { useLogout } from "../hooks/useLogout";
 import Nav from "./Nav";
 import Button from "./utils/Button";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { logout } = useLogout();
 
   return (
-    // <header role="banner" aria-labelledby="odinbook-logo" className="shadow-sm sticky top-0 w-full bg-white flex items-center justify-evenly lg:justify-between">
-
     <header role="banner" aria-labelledby="odinbook-logo" className="shadow-md sticky top-0 w-full bg-white grid grid-cols-1 lg:grid-cols-3 items-center z-10">
-
-      <div className="items-center hidden lg:flex justify-self-start">
+      <Link to="/" className="items-center hidden lg:flex justify-self-start">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 105.22 83.68" className="w-20">
-          <title id="odinbook-logo" aria-labelledby="optomrx-logo">Odinbook</title>
+          <title id="odinbook-logo" aria-labelledby="odinbook-logo">Odinbook</title>
           <g id="Layer_2" data-name="Layer 2">
             <g id="Layer_1-2" data-name="Layer 1">
               <rect fill="#51557d" className="cls-1" width="105.22" height="83.68" />
@@ -22,14 +20,13 @@ const Header = () => {
           </g>
         </svg>
         <h1 className="font-semibold text-3xl text-plum-500 ml-4">odinbook</h1>
-      </div>
+      </Link>
 
       <Nav />
       {/* Dropdown profile/settings menu here */}
 
       {/* Separate logout button on the far right for large screens only (ordinarily part of nav menu) */}
       <div className="hidden lg:block justify-self-end mr-4">
-        {/* Icon */}
         <Button design="none" onClick={logout}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-8">
             <path fill="#51557d" d="M160 416H96c-17.67 0-32-14.33-32-32V128c0-17.67 14.33-32 32-32h64c17.67 0 32-14.33 32-32S177.7 32 160 32H96C42.98 32 0 74.98 0 128v256c0 53.02 42.98 96 96 96h64c17.67 0 32-14.33 32-32S177.7 416 160 416zM502.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L402.8 224H192C174.3 224 160 238.3 160 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C515.1 266.1 515.1 245.9 502.6 233.4z"/>
