cb2f590a385e8d9ce46b64159d8e3e1682fd918f
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-23 14:30:51 +1030

    Fix disabled styling for all friend buttons

--- src/components/utils/Button.js
+++ src/components/utils/Button.js
@@ -19,18 +19,30 @@ const Button = ({
         break;
 
       case 'secondary-lg':
+        CSS = 'flex items-center justify-center w-full px-4 py-2 font-semibold bg-white text-plum-400 text-md md:text-lg shadow-md border border-plum-400 hover:bg-plum-50 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-plum-300/30'
+        break;
+
+      case 'teal-lg':
         CSS = 'flex items-center justify-center w-full px-4 py-2 font-semibold bg-teal-650 border border-teal-650  shadow-md text-white text-md md:text-lg hover:bg-teal-550 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-teal-650/30'
         break;
 
       case 'primary':
-        CSS = 'flex items-center justify-center w-full px-2 py-1 bg-plum-400 border border-plum-400 text-white shadow-md hover:bg-plum-300 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-plum-300/30 disabled:bg-gray-100 disabled:text-gray-500 border-gray-100 disabled:shadow-none'
+        CSS = 'flex items-center justify-center w-full px-2 py-1 bg-plum-400 border border-plum-400 text-white shadow-md hover:bg-plum-300 hover:border-plum-300 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-plum-300/30 disabled:bg-gray-100 disabled:text-gray-400 disabled:shadow-none disabled:border-gray-100'
+        break;
+    
+      case 'secondary':
+        CSS = 'flex items-center justify-center w-full px-2 py-1 bg-gray-200 border border-gray-200 text-black shadow-sm hover:bg-gray-200/80 hover:border-gray-200/10 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-gray-300 disabled:bg-gray-100 disabled:text-gray-500 disabled:shadow-none'
+        break;
+
+      case 'danger':
+        CSS = 'flex items-center justify-center w-full px-2 py-1 bg-red-200 border border-gray-200 text-black shadow-sm hover:bg-gray-200/80 hover:border-gray-200/10 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-gray-300/30 disabled:bg-gray-100 disabled:text-gray-500 disabled:shadow-none'
         break;
 
       case 'ghost':
-        CSS = 'flex items-center justify-center w-full px-2 py-1 bg-white text-plum-400 shadow-md border border-plum-400 hover:bg-plum-50 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-plum-300/30'
+        CSS = 'flex items-center justify-center w-full px-2 py-1 bg-white text-plum-600 shadow-md border border-plum-400 hover:bg-plum-50 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-plum-300/30 disabled:bg-gray-100 disabled:text-gray-400 disabled:shadow-none disabled:border-gray-100'
         break;
 
-      case 'secondary':
+      case 'teal':
         CSS = 'flex items-center justify-center w-full px-2 py-1 bg-teal-650 border border-teal-650  shadow-md text-white hover:bg-teal-550 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-teal-650/30'
         break;
     
