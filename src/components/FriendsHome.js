c5b2023933dbaa0544289686f68bfebbe5e9ae39
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-23 13:21:36 +1030

    Adjust userList to only show users not already in friends array

--- src/components/FriendsHome.js
+++ src/components/FriendsHome.js
@@ -38,7 +38,9 @@ const FriendsHome = () => {
 
           <section className="bg-white my-8 md:m-4 lg:bg-transparent">
             <h2 className="text-xl font-bold p-4 lg:pb-2 lg:pt-8 lg:mt-8 lg:border-t lg:border-gray-300">Find new friends</h2>
-            <UserList />
+            {friends && (
+              <UserList userFriends={friends}/>
+            )}
           </section>
         </main>
       </div>
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -2,17 +2,27 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import { useFetchGet } from "../hooks/useFetchGet"
 import FriendCard from "./FriendCard";
                            
-const UserList = () => {
+const UserList = ({ userFriends }) => {
   const { data: users } = useFetchGet(`http://localhost:3000/api/users`);
-  const { user } = useAuthContext();
+  const { user: currentUser } = useAuthContext();
+
+  // Returns false if a user is either involved in the current user's friends array (request or friend), or is the current user themselves. Should only be called once users data is available
+  const isRelatedUser = (userId) => {
+    // Perform array operations to manipulate the userFriends object into a single depth array of user IDs
+    const flatFriends = [userFriends.acceptedFriends, userFriends.incomingRequests, userFriends.outgoingRequests].flat();
+    const userFriendIds = flatFriends.map((friend) => friend.user._id);
+
+    userFriendIds.push(currentUser._id);
+
+    return userFriendIds.some((id) => id === userId);
+  }
 
   return (
     <div>
-      {users && (
+      {users &&(
         <div className="flex flex-wrap items-center justify-start">
           {users.users.map((userDetails) => {
-            // Ensure that the current user is not rendered in the list of all users
-            if (userDetails._id !== user._id) {
+            if (!isRelatedUser(userDetails._id)) {
               return (<FriendCard friendData={userDetails} type="user" key={userDetails._id}/>)
             }
           })}
