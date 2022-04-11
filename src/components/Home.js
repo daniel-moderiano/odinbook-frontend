7b16f25afc888efcf563ac34970c1ec58ed77954
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-11 16:37:26 +0930

    Address FB back btn auth bug by adding ErrorBoundary around homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,29 +4,30 @@ import SideMenu from "./SideMenu";
 import SideFooter from "./SideFooter";
 import CreatePost from "./CreatePost";
 import { useState } from "react";
+import ErrorBoundary from "./utils/ErrorBoundary";
 
 const Home = () => {
   // A method of updating the Feed when a new post is created, updated, or deleted. Update or change the feedKey state from within a child component to re-render the Feed component
-  const [feedKey, setFeedKey] = useState(0)
+  const [feedKey, setFeedKey] = useState(0);
 
   return (
-    <div>
+    <ErrorBoundary>
       <Header />
-      <div className="w-full flex justify-center mt-6">
-        <div className="grid grid-cols-1 lg:grid-cols-[350px_1fr] md:gap-x-8 2xl:grid-cols-[350px_1fr_350px] lg:mx-8">
-          <div className="hidden lg:flex">
-            <SideMenu />
-          </div>
-          <main className="max-w-[640px] col-start-1 lg:col-start-2 flex flex-col justify-center self-center w-full lg:max-w-[550px] xl:max-w-[640px] md:min-w-[640px] lg:min-w-[550px] xl:min-w-[640px]">
-            <CreatePost updateFeed={setFeedKey}/>
-            <Feed updateFeed={setFeedKey} key={feedKey}/>
-          </main>
-          <div className="hidden 2xl:flex">
-            <SideFooter />
+        <div className="w-full flex justify-center mt-6">
+          <div className="grid grid-cols-1 lg:grid-cols-[350px_1fr] md:gap-x-8 2xl:grid-cols-[350px_1fr_350px] lg:mx-8">
+            <div className="hidden lg:flex">
+              <SideMenu />
+            </div>
+            <main className="max-w-[640px] col-start-1 lg:col-start-2 flex flex-col justify-center self-center w-full lg:max-w-[550px] xl:max-w-[640px] md:min-w-[640px] lg:min-w-[550px] xl:min-w-[640px]">
+              <CreatePost updateFeed={setFeedKey}/>
+              <Feed updateFeed={setFeedKey} key={feedKey}/>
+            </main>
+            <div className="hidden 2xl:flex">
+              <SideFooter />
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+    </ErrorBoundary>
   )
 }
 
---
+++ src/components/utils/ErrorBoundary.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from './Button';
+
+// Code taken from the React Error Boundary docs with small adjustments
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    // Set initial error state to false, with no message
+    this.state = { 
+      hasError: false,
+      errorMsg: '',
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update hasError state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    // Set the error message for any custom message handling in UI
+    this.setState({ errorMsg: error.message });
+    console.log(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Fallback UI to render here. Because this is targetting the FB auth back btn error, a reload page btn is included
+      return (
+        <div className='w-full h-full bg-gray-100 flex items-center justify-center'>
+          <div className="flex flex-col items-center justify-center">
+            <h1 className='text-3xl font-semibold text-plum-500 mb-4'>An error occurred</h1>
+            <Button design="secondary" customStyles="w-36" onClick={() => window.location.reload()}>Reload page</Button>
+          </div>
+        </div>
+      )
+    }
+    return this.props.children; 
+  }
+}
+
+export default ErrorBoundary;
