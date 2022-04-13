aea3dca19840234e1727e9e8ba77fb01cb758373
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-13 18:29:21 +0930

    Abstract entire test login button and logic to separate component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,17 +2,15 @@ import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
 import Input from "./utils/Input";
 import Button from "./utils/Button";
-import StyledLink from "./utils/StyledLink";
 import { useErrorToast } from '../hooks/useErrorToast';
-import { useTestLogin } from '../hooks/useTestLogin';
+import TestLogin from "./TestLogin";
 
 const Login = () => {
   const { login, error, formError, loading } = useLogin();
-  const { testLogin, testError, testLoading } = useTestLogin();
 
   // All non-401 and non-400 errors
   useErrorToast(error, (error && error.errorMsg));
-  useErrorToast(testError, 'An unknown error occurred while logging in')
+
 
   const [formData, setFormData] = useState({
     email: '',
@@ -107,20 +105,7 @@ const Login = () => {
                 <span>Log in with Facebook</span>
               </Button>
 
-              <div>
-                <StyledLink to="/signup" design="btn-secondary" customStyles="w-56 mt-12 font-semibold">Create new account</StyledLink>
-                <button 
-                  onClick={testLogin} 
-                  type="button" 
-                  className="font-semibold mt-3 flex items-center justify-center w-full px-2 py-1 bg-white text-teal-750 shadow-sm border border-teal-750 hover:bg-plum-50 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-teal-550/30 disabled:bg-gray-100 disabled:text-gray-400 disabled:shadow-none disabled:border-gray-100">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-4 mr-2" >
-                    <path fill="#218381" d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"/>
-                    </svg>
-                  <span>
-                    {testLoading ? 'Logging in...': 'Try a test account'}
-                  </span>
-                </button>
-              </div>
+              <TestLogin />
             </div>
 
           </div>
---
+++ src/components/TestLogin.js
@@ -0,0 +1,28 @@
+import StyledLink from "./utils/StyledLink"
+import { useTestLogin } from "../hooks/useTestLogin";
+import { useErrorToast } from "../hooks/useErrorToast";
+
+const TestLogin = () => {
+  const { testLogin, testError, testLoading } = useTestLogin();
+
+  useErrorToast(testError, 'An unknown error occurred while logging in');
+
+  return (
+    <div>
+      <StyledLink to="/signup" design="btn-secondary" customStyles="w-56 mt-12 font-semibold">Create new account</StyledLink>
+      <button 
+        onClick={testLogin} 
+        type="button" 
+        className="font-semibold mt-3 flex items-center justify-center w-full px-2 py-1 bg-white text-teal-750 shadow-sm border border-teal-750 hover:bg-plum-50 focus:outline-none focus:ring ring-transparent ring-offset-2 ring-offset-teal-550/30 disabled:bg-gray-100 disabled:text-gray-400 disabled:shadow-none disabled:border-gray-100">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-4 mr-2" >
+          <path fill="#218381" d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"/>
+          </svg>
+        <span>
+          {testLoading ? 'Logging in...': 'Try a test account'}
+        </span>
+      </button>
+    </div>
+  )
+}
+
+export default TestLogin;
