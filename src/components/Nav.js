f692ebc3b19c1cb8126faadb86d8f9e44b0547e1
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-13 22:47:14 +0930

    Add all nav icons as components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,10 @@ import Button from "./utils/Button";
 import { useLogout } from "../hooks/useLogout";
 import { Link } from 'react-router-dom';
 import { useAuthContext } from "../hooks/useAuthContext";
+import HomeIcon from './icons/HomeIcon';
+import FriendsIcon from "./icons/FriendsIcon";
+import ProfileIcon from "./icons/ProfileIcon";
+import LogoutIcon from './icons/LogoutIcon';
 
 const Nav = () => {
   const { logout } = useLogout();
@@ -13,10 +17,7 @@ const Nav = () => {
         <li role="menuitem" >
           <Link to="/">
             <div className="py-1 px-4 hover:bg-gray-100 flex flex-col items-center justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-7 h-8">
-                <title id="Home">Home</title>
-                <path fill="#51557d" d="M511.8 287.6L512.5 447.7C512.5 450.5 512.3 453.1 512 455.8V472C512 494.1 494.1 512 472 512H456C454.9 512 453.8 511.1 452.7 511.9C451.3 511.1 449.9 512 448.5 512H392C369.9 512 352 494.1 352 472V384C352 366.3 337.7 352 320 352H256C238.3 352 224 366.3 224 384V472C224 494.1 206.1 512 184 512H128.1C126.6 512 125.1 511.9 123.6 511.8C122.4 511.9 121.2 512 120 512H104C81.91 512 64 494.1 64 472V360C64 359.1 64.03 358.1 64.09 357.2V287.6H32.05C14.02 287.6 0 273.5 0 255.5C0 246.5 3.004 238.5 10.01 231.5L266.4 8.016C273.4 1.002 281.4 0 288.4 0C295.4 0 303.4 2.004 309.5 7.014L416 100.7V64C416 46.33 430.3 32 448 32H480C497.7 32 512 46.33 512 64V185L564.8 231.5C572.8 238.5 576.9 246.5 575.8 255.5C575.8 273.5 560.8 287.6 543.8 287.6L511.8 287.6z" />
-              </svg>
+              <HomeIcon iconFill="#51557d" iconStyles="w-7 h-8"/>
               <span className="text-xs">Home</span>
             </div>
           </Link>
@@ -24,29 +25,22 @@ const Nav = () => {
         <li role="menuitem">
           <Link to="/friends">
             <div className="py-1 px-4 hover:bg-gray-100 flex flex-col items-center justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="w-7 h-8">
-                <path fill="#51557d" d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3c-95.73 0-173.3 77.6-173.3 173.3C0 496.5 15.52 512 34.66 512H413.3C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM479.1 320h-73.85C451.2 357.7 480 414.1 480 477.3C480 490.1 476.2 501.9 470 512h138C625.7 512 640 497.6 640 479.1C640 391.6 568.4 320 479.1 320zM432 256C493.9 256 544 205.9 544 144S493.9 32 432 32c-25.11 0-48.04 8.555-66.72 22.51C376.8 76.63 384 101.4 384 128c0 35.52-11.93 68.14-31.59 94.71C372.7 243.2 400.8 256 432 256z"/>
-              </svg>
+              <FriendsIcon iconFill="#51557d" iconStyles="w-7 h-8"/>
               <span className="text-xs">Friends</span>
             </div>
           </Link>
         </li>
         <li role="menuitem">
           <Link to={`/profile/${user._id}`}>
-            {/* Consider replacing this icon with profile pic (anon pic if none provided) */}
             <div className="py-1 px-4 hover:bg-gray-100 flex flex-col items-center justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-7 h-8">
-                <path fill="#51557d" d="M512 32H64C28.65 32 0 60.65 0 96v320c0 35.35 28.65 64 64 64h448c35.35 0 64-28.65 64-64V96C576 60.65 547.3 32 512 32zM176 128c35.35 0 64 28.65 64 64s-28.65 64-64 64s-64-28.65-64-64S140.7 128 176 128zM272 384h-192C71.16 384 64 376.8 64 368C64 323.8 99.82 288 144 288h64c44.18 0 80 35.82 80 80C288 376.8 280.8 384 272 384zM496 320h-128C359.2 320 352 312.8 352 304S359.2 288 368 288h128C504.8 288 512 295.2 512 304S504.8 320 496 320zM496 256h-128C359.2 256 352 248.8 352 240S359.2 224 368 224h128C504.8 224 512 231.2 512 240S504.8 256 496 256zM496 192h-128C359.2 192 352 184.8 352 176S359.2 160 368 160h128C504.8 160 512 167.2 512 176S504.8 192 496 192z"/>
-              </svg>
+              <ProfileIcon iconFill="#51557d" iconStyles="w-7 h-8"/>
               <span className="text-xs">Profile</span>
             </div>
           </Link>
         </li>
         <li role="menuitem" className="lg:hidden">
           <Button design="none" onClick={logout} customStyles="py-1 px-4 hover:bg-gray-100 flex flex-col items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-7 h-8">
-              <path fill="#51557d" d="M160 416H96c-17.67 0-32-14.33-32-32V128c0-17.67 14.33-32 32-32h64c17.67 0 32-14.33 32-32S177.7 32 160 32H96C42.98 32 0 74.98 0 128v256c0 53.02 42.98 96 96 96h64c17.67 0 32-14.33 32-32S177.7 416 160 416zM502.6 233.4l-128-128c-12.51-12.51-32.76-12.49-45.25 0c-12.5 12.5-12.5 32.75 0 45.25L402.8 224H192C174.3 224 160 238.3 160 256s14.31 32 32 32h210.8l-73.38 73.38c-12.5 12.5-12.5 32.75 0 45.25s32.75 12.5 45.25 0l128-128C515.1 266.1 515.1 245.9 502.6 233.4z"/>
-            </svg>
+            <LogoutIcon iconFill="#51557d" iconStyles="w-7 h-8"/>
             <span className="text-xs">Log out</span>
           </Button>
         </li>
--- src/components/Post.js
+++ src/components/Post.js
@@ -11,6 +11,8 @@ import { useAuthContext } from '../hooks/useAuthContext';
 import PostMenu from './PostMenu';
 import DeletePostModal from './DeletePostModal';
 import EditPostModal from './EditPostModal';
+import EllipsisIcon from './icons/EllipsisIcon';
+import CommentIcon from './icons/CommentIcon'
 
 const Post = ({ post, updateFeed }) => {
   const { user } = useAuthContext();
@@ -90,9 +92,7 @@ const Post = ({ post, updateFeed }) => {
         <div className='relative'>
           {post.user._id === user._id && (
             <button data-id="post-menu" onClick={toggleMenu} data-testid="menu" className='px-2 py-1 rounded hover:bg-gray-100 active:bg-gray-200'>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='w-4 pointer-events-none'>
-                <path d="M120 256C120 286.9 94.93 312 64 312C33.07 312 8 286.9 8 256C8 225.1 33.07 200 64 200C94.93 200 120 225.1 120 256zM280 256C280 286.9 254.9 312 224 312C193.1 312 168 286.9 168 256C168 225.1 193.1 200 224 200C254.9 200 280 225.1 280 256zM328 256C328 225.1 353.1 200 384 200C414.9 200 440 225.1 440 256C440 286.9 414.9 312 384 312C353.1 312 328 286.9 328 256z"/>
-              </svg>
+              <EllipsisIcon iconFill="#000" iconStyles='w-4 pointer-events-none' />
             </button>
           )}
           
@@ -127,9 +127,7 @@ const Post = ({ post, updateFeed }) => {
       <div className={`flex items-center justify-evenly mx-4 border-t pt-1 ${showComments && 'border-b mb-6 py-1'}`}>
         <LikeBtn post={post} setLocalLike={setLocalLike}/>
         <Button customStyles="flex items-middle justify-center text-gray-500 font-medium hover:bg-gray-100 py-2 rounded w-full" onClick={() => setShowComments(true)}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='w-6 mr-2'>
-            <path fill='#6b7280' d="M256 32C114.6 32 .0272 125.1 .0272 240c0 47.63 19.91 91.25 52.91 126.2c-14.88 39.5-45.87 72.88-46.37 73.25c-6.625 7-8.375 17.25-4.625 26C5.818 474.2 14.38 480 24 480c61.5 0 109.1-25.75 139.1-46.25C191.1 442.8 223.3 448 256 448c141.4 0 255.1-93.13 255.1-208S397.4 32 256 32zM256.1 400c-26.75 0-53.12-4.125-78.38-12.12l-22.75-7.125l-19.5 13.75c-14.25 10.12-33.88 21.38-57.5 29c7.375-12.12 14.37-25.75 19.88-40.25l10.62-28l-20.62-21.87C69.82 314.1 48.07 282.2 48.07 240c0-88.25 93.25-160 208-160s208 71.75 208 160S370.8 400 256.1 400z"/>
-          </svg>
+          <CommentIcon iconFill='#6b7280' iconStyles='w-6 mr-2'/>
           Comment
         </Button>
       </div>
