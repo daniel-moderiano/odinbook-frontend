486267a3d49f12e4f253509b5185d200f751f136
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-18 17:38:07 +0930

    Fix friends tab styling

--- src/components/ProfileFriendsTab.js
+++ src/components/ProfileFriendsTab.js
@@ -23,8 +23,8 @@ const ProfileFriendsTab = ({ profileUser }) => {
       )}
 
       {loading && (
-        <div className="mt-4 flex flex-wrap gap-x-4 gap-y-3 sm:gap-x-5 md:gap-x-10 lg:gap-3 sm:justify-between">
-          {[0, 1, 2, 3].map((friend, index) => {
+        <div className="mt-4 flex flex-wrap gap-x-4 gap-y-3 sm:gap-x-5 md:gap-x-4 lg:gap-3">
+          {[0, 1, 2, 3, 4].map((friend, index) => {
             return <SkeletonFriendTileLarge key={index}/>
           })}
         </div>
@@ -35,14 +35,14 @@ const ProfileFriendsTab = ({ profileUser }) => {
           {/* Friends array may exist but the user may not yet have any accepted friends. Hence this is checked here */}
           {friends.acceptedFriends.length > 0 ? (
             <>
-              <div className="flex flex-wrap gap-x-4 gap-y-3 sm:gap-x-5 md:gap-x-10 lg:gap-3 sm:justify-between mt-2">
+              <div className="flex flex-wrap gap-x-4 gap-y-3 sm:gap-x-5 md:gap-x-4 lg:gap-3 mt-2">
                 {friends.acceptedFriends.map((friend, index) => (
                   <div key={friend.user._id} className="flex flex-col items-center justify-start lg:flex-row lg:rounded lg:border lg:border-gray-200 lg:p-2 lg:w-[415px]">
-                    <Link to={`/profile/${friend.user._id}`} className="hover:opacity-95 active:opacity-100">
-                      <ProfilePic imgUrl={friend.user.profilePic ? friend.user.profilePic.imageUrl : null} styles="w-24 h-24 sm:w-28 sm:h-28 lg:w-24 lg:h-24 lg:mr-4 rounded"/>
+                    <Link to={`/profile/${friend.user._id}`} className="hover:opacity-95 active:opacity-100 lg:mr-4 outline-plum-600 outline-offset-2">
+                      <ProfilePic imgUrl={friend.user.profilePic ? friend.user.profilePic.imageUrl : null} styles="w-24 h-24 sm:w-28 sm:h-28 lg:w-24 lg:h-24 rounded"/>
                     </Link>
                     <div>
-                      <Link to={`/profile/${friend.user._id}`} className="block text-xs sm:text-sm lg:text-lg font-semibold mt-2 w-full text-center max-w-[96px] sm:max-w-[112px] lg:max-w-xs lg:text-left hover:underline">{friend.user.fullName}</Link>
+                      <Link to={`/profile/${friend.user._id}`} className="block text-xs sm:text-sm lg:text-lg font-semibold mt-2 w-full text-center max-w-[96px] sm:max-w-[112px] lg:max-w-xs lg:text-left hover:underline outline-plum-600">{friend.user.fullName}</Link>
                     </div>
                   </div>
                 ))}
