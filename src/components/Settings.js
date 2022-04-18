dc0bfe7a2bb14eefc575e00433183966a4a20400
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-18 14:11:12 +0930

    Add responsive design to settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -10,18 +10,19 @@ const Settings = () => {
   const { user } = useAuthContext();
   const { deleteAccount, response, loading, error } = useDeleteAccount();
 
+  // Set up notifications
   useErrorToast(error, (error && error.errorMsg));
   useSuccessToast(response, 'Account deleted successfully');
 
   return (
-    <div className="h-full flex flex-col justify-between">
+    <div className="h-full flex flex-col justify-between items-center">
       <Header />
-      <main className="mt-[74px] lg:mt-[82px] flex flex-col bg-white shadow-sm p-4 pb-3">
+      <main className="mt-[74px] lg:mt-[82px] flex flex-col bg-white shadow-sm p-4 pb-3 md:p-6 md:pb-4 max-w-[640px] lg:max-w-3xl">
         <h1 className="text-2xl font-bold lg:pb-2 pb-1">Settings</h1>
         <section className="py-4">
           <h2 className="border-b mb-3 pb-2 text-lg">Delete your account</h2>
-          <p className="text-sm mb-4">Once you delete your account, it cannot be retrieved. All posts and comments you have made will remain visible to other users.</p>
-          <Button customStyles="max-w-[180px]" design="danger" onClick={() => deleteAccount(user._id)}>
+          <p className="text-sm mb-4">Once you delete your account, it cannot be retrieved. All traces of you will be removed from Odinbook, including likes, comments, and posts. Proceed with caution.</p>
+          <Button customStyles="max-w-[180px]" design="danger" onClick={() => deleteAccount(user._id)} disabled={user._id === '6253eafa7c5f03b0906cc7b5'}>
             {loading ? (
               'Deleting...'
             ) : (
