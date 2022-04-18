e16b48b38bac96d0da895f7e160eb38278ae9d8d
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-18 19:52:13 +0930

    Finalise all focus styles and overall site design

--- src/components/SideFooter.js
+++ src/components/SideFooter.js
@@ -6,7 +6,7 @@ const SideFooter = () => {
       <div className="flex flex-col items-center border border-gray-300/80 rounded-md py-4">
         <h2 className="font-semibold text-2xl text-gray-600 mb-0.5">odinbook</h2>
         <p className="text-sm text-gray-500 mb-0.5">&copy; Daniel Moderiano 2022</p>
-        <a href="https://github.com">
+        <a href="https://github.com" className="outline-gray-500">
           <GithubIcon iconFill="#1f2937" iconStyles="w-8 p-1 opacity-60 hover:opacity-100" />
         </a>
       </div>
