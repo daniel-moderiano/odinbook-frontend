935d2a32a46e2031fe20cf1cc96d7d1e2114b496
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-19 16:58:59 +0930

    Add accessible keyboard menu nav for arrow keys in dropdown menu component

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,24 +1,93 @@
 import { useLogout } from "../hooks/useLogout";
 import { Link } from 'react-router-dom'
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useEffect } from "react";
 
 const DropdownMenu = ({ closeMenu }) => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
 
+  // Focus the first menu item when the menu is rendered
+  // useEffect(() => {
+  //   const firstItem = document.querySelector('#first-item');
+  //   firstItem.focus();
+  // }, []);
+
+  // Trap focus within the menu
+  useEffect(() => {
+    const menu = document.querySelector('#dropdown');
+    // Grab all focusable elements within the menu
+    const menuItems = menu.querySelectorAll('[role="menuitem"]');
+
+    // Focus the first menu item when the menu is first opened
+    menuItems[0].focus();
+
+    const handleKeyPress = (e) => {
+      let currentFocus; 
+
+      // Find currently focused menu item
+      for (let i = 0; i < menuItems.length; i++) {
+        if (menuItems[i] === document.activeElement) {
+          currentFocus = i;
+        }
+      }
+
+      console.log(currentFocus);
+
+      switch (e.key) {
+        case 'ArrowDown':
+          e.preventDefault();
+          if (currentFocus < menuItems.length - 1) {
+            currentFocus++;
+          } else {
+            currentFocus = 0;
+          }
+          menuItems[currentFocus].focus();
+          break;
+
+        case 'ArrowUp':
+          e.preventDefault();
+          if (currentFocus === 0) {
+            currentFocus = menuItems.length - 1;
+          } else {
+            currentFocus--;
+          }
+          menuItems[currentFocus].focus();
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    menu.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      menu.removeEventListener('keydown', handleKeyPress);
+    }
+  }, []);
+
   return (
-    <div data-testid="dropdown" data-id="dropdown" className="rounded p-1 absolute top-[53px] right-0 shadow-lg w-36 bg-white text-left lg:top-[60px]">
-      <Link to={`/profile/${user._id}`} onClick={() => {
+    <ul aria-labelledby="me-dropdown" id="dropdown" role="menu" data-testid="dropdown" data-id="dropdown" className="rounded p-1 absolute top-[53px] right-0 shadow-lg w-36 bg-white text-left lg:top-[60px]">
+      <li role="none">
+        <Link id="first-item" role="menuitem" to={`/profile/${user._id}`} onClick={() => {
         closeMenu();
-      }} className="bg-white py-2 px-3 hover:bg-gray-100 w-full text-left rounded block outline-plum-500 outline-offset-[-1px]">View profile</Link>
-      <Link to="/settings" onClick={() => {
+        }} className="bg-white py-2 px-3 hover:bg-gray-100 w-full text-left rounded block outline-plum-500 outline-offset-[-1px]">View profile</Link>
+      </li>
+      <li role="none">
+        <Link role="menuitem" to="/settings" onClick={() => {
         closeMenu();
-      }} className="bg-white py-2 px-3 hover:bg-gray-100 w-full text-left rounded block outline-plum-500 outline-offset-[-1px]">Settings</Link>
-       <button onClick={() => {
-        closeMenu();
-        logout();
-      }} className="bg-white py-2 px-3 hover:bg-gray-100 w-full text-left rounded outline-plum-500 outline-offset-[-1px]">Log out</button>
-    </div>
+        }} className="bg-white py-2 px-3 hover:bg-gray-100 w-full text-left rounded block outline-plum-500 outline-offset-[-1px]">Settings</Link>
+      </li>
+      <li role="none">
+        <button role="menuitem" onClick={() => {
+          closeMenu();
+          logout();
+          }} className="bg-white py-2 px-3 hover:bg-gray-100 w-full text-left rounded outline-plum-500 outline-offset-[-1px]">
+          Log out
+        </button>
+      </li>
+    </ul>
   )
 }
 
--- src/components/buttons/MeButton.js
+++ src/components/buttons/MeButton.js
@@ -10,10 +10,10 @@ const MeButton = () => {
 
   return (
     <div className="relative" data-testid="outside">
-      <button role="menuitem" data-testid="user-menu" data-id="dropdown" onClick={toggleMenu} className="w-full pb-[2px] pt-[4px] px-4 lg;px-12 lg:pb-0 lg:pt-[2px] lg:rounded flex flex-col items-center justify-center hover:bg-gray-100 outline-plum-600 outline-offset-[-2px] lg:outline-offset-[-1px]">
+      <button aria-controls="dropdown" aria-haspopup="true" aria-expanded={showMenu} role="menuitem" data-testid="user-menu" data-id="dropdown" onClick={toggleMenu} className="w-full pb-[2px] pt-[4px] px-4 lg;px-12 lg:pb-0 lg:pt-[2px] lg:rounded flex flex-col items-center justify-center hover:bg-gray-100 outline-plum-600 outline-offset-[-2px] lg:outline-offset-[-1px]">
         <ProfilePic image={user.profilePic && user.profilePic} styles="mb-0.5 w-6 h-6 lg:w-8 lg:h-8 rounded-full pointer-events-none"/>
         <div className='flex items-center justify-center pointer-events-none'>
-          <span className="text-xs">Me</span>
+          <span id="me-dropdown" className="text-xs">Me</span>
           <CaretIcon iconStyles='w-2.5 ml-0.5 -mt-0.5 pointer-events-none' iconFill="#51557d"/>
         </div>
       </button>
