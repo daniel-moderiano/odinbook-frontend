3398fa9ab680b9b75004acedc545682744f04bdf
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-22 22:31:14 +0930

    Remove unused skeleton elements

--- src/components/skeletons/SkeletonElement.js
+++ src/components/skeletons/SkeletonElement.js
@@ -1,5 +1,4 @@
-// Base components of a larger skeleton layout. Can accept ype prop for simple building blocks of common skeleton layouts.
-
+// Base components of a larger skeleton layouts. Can accept type prop for simple building blocks of common skeleton layouts.
 const SkeletonElement = ({ type }) => {
   let classStyles = 'bg-gray-200 ';
 
@@ -10,10 +9,6 @@ const SkeletonElement = ({ type }) => {
 
     case 'text-sm':
       classStyles += 'w-full h-2 rounded my-2';
-      break;
-
-    case 'title':
-      classStyles += 'w-6/12 h-8 mb-6 rounded my-4';
       break;
 
     case 'avatar':
@@ -34,14 +29,6 @@ const SkeletonElement = ({ type }) => {
 
     case 'avatar-full':
       classStyles += 'w-20 h-20 rounded-full my-4 shrink-0 lg:w-36 lg:h-36 lg:mt-7';
-      break;
-
-    case 'thumbnail':
-      classStyles += 'w-10 h-10 rounded my-4';
-      break;
-
-    case 'button':
-      classStyles += 'w-28 h-8 my-4';
       break;
 
     default:
